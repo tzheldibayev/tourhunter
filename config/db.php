1833fc89ca90cf4dacc83455a8bf540141173e7d
talgat065 <talgat06585@gmail.com>
2020-03-27 18:09:57 +0600

    feat(): add .env-example file

--- config/db.php
+++ config/db.php
@@ -2,10 +2,10 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
+    'dsn' => getenv('DB_DSN'),
+    'username' => getenv('DB_USERNAME'),
+    'password' => getenv('DB_PASSWORD'),
+    'charset' => 'utf8mb4',
 
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
--- web/index.php
+++ web/index.php
@@ -7,6 +7,25 @@ defined('YII_ENV') or define('YII_ENV', 'dev');
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
+// Load .env configuration
+$repository = Dotenv\Repository\RepositoryBuilder::create()
+    ->withReaders([
+        new Dotenv\Repository\Adapter\EnvConstAdapter(),
+    ])
+    ->withWriters([
+        new Dotenv\Repository\Adapter\EnvConstAdapter(),
+        new Dotenv\Repository\Adapter\PutenvAdapter(),
+    ])
+    ->immutable()
+    ->make();
+$dotenv = Dotenv\Dotenv::create($repository, __DIR__.'../');
+$dotenv->required('DB_DSN')->notEmpty();
+$dotenv->load();
+
 $config = require __DIR__ . '/../config/web.php';
 
 (new yii\web\Application($config))->run();
+
+
+// для предотвращения скачивания .env файла выносим его за пределы DOCUMENT_ROOT (/web/), т.е. в корень проекта
+// если есть возможность, так же добавить запрет на доступ к .env файлу в конфиге веб сервера
